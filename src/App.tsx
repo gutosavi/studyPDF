021b6807194ec3d8a24bd0db9a2152b0867565de
Gustavo Savi <gutosavi@hotmail.com>
2026-06-15 14:02:22 -0300

    feat: add application routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import UploadArea from './app/components/UploadArea';
+import ChatArea from './app/components/ChatArea';
 import './App.css';
 
 function App() {
-  return <>Hello World</>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<UploadArea />} />
+          <Route path="/chat" element={<ChatArea />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;

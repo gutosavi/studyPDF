1505b60cb3636860f99a3bf4aa49efeb028587f1
Gustavo Savi <gutosavi@hotmail.com>
2026-06-16 14:09:02 -0300

    feat: set pages routes

---
+++ src/routes/AppRoutes.tsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import HomePage from '../pages/HomePage';
+import ChatPage from '../pages/ChatPage';
+
+export function AppRoutes() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/chat" element={<ChatPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}

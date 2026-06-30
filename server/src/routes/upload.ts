914d95dec23b57aff56dfc8b7dbf95e9cebddb80
Gustavo Savi <gutosavi@hotmail.com>
2026-06-30 19:38:23 -0300

    feat: cria nova rota de upload

---
+++ server/src/routes/upload.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+
+import uploadController from '../controllers/uploadController';
+
+const router = express.Router();
+
+router.post('/upload', uploadController);
+
+export default router;

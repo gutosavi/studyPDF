ca85fdea38d8f5055e641f4c5f6f9ae3bcc3fdf4
Gustavo Savi <gutosavi@hotmail.com>
2026-07-02 14:56:37 -0300

    feat: implementa hook [useUploadFile.ts]

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,64 +1,15 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
 import UploadArea from '../app/components/UploadArea';
-import extractPdfData from '../features/pdf/extractPdfData';
-
-interface DocumentInfo {
-  name: string;
-  size: string;
-  uploadDate: string;
-  countPage: number;
-  pageContents: {
-    pageNumber: number;
-    text: string;
-  }[];
-}
+import { useNavigate } from 'react-router-dom';
+import { useUploadFile } from '../hooks/useUploadFile';
 
 const HomePage = () => {
-  const [document, setDocument] = React.useState<DocumentInfo | null>(null);
   const navigate = useNavigate();
-
-  const handleFileUpload = async (file: File) => {
-    const fileSizeInMB = (file.size / (1024 * 1024)).toFixed(2);
-    const uploadDate = new Date().toLocaleDateString('pt-BR');
-    const pdfData = await extractPdfData(file);
-
-    setDocument({
-      name: file.name,
-      size: `${fileSizeInMB} MB`,
-      uploadDate,
-      countPage: pdfData.totalPages,
-      pageContents: pdfData.pagesData,
-    });
-  };
+  const { document, handleFileUpload, sendDocumentToServer } = useUploadFile();
 
   React.useEffect(() => {
-    const sendDocumentToServer = async (document: DocumentInfo | null) => {
-      if (!document) return;
-
-      const documentText = document.pageContents.map((p) => p.text).join('\n');
-
-      try {
-        const response = await fetch('http://localhost:3000/upload', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ fileName: document.name, documentText }),
-        });
-
-        if (!response.ok) {
-          throw new Error(`Erro na requisição ${response.status}`);
-        }
-
-        const data = await response.json();
-        console.log('Arquivo enviado ao backend', data);
-      } catch (error) {
-        console.error('Erro no upload', error);
-      }
-    };
     sendDocumentToServer(document);
-  });
+  }, [sendDocumentToServer, document]);
 
   React.useEffect(() => {
     if (document) {

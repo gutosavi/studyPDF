b180defcf4207b05f791ccdc1d1815e9bce065c3
Gustavo Savi <gutosavi@hotmail.com>
2026-07-08 09:58:23 -0300

    debug: testa rotas para ver se API está funcionando

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,6 +10,11 @@ const PORT = 3000;
 app.use(cors());
 app.use(express.json());
 
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+
 app.use(uploadRouter);
 app.use(chatRouter);
 

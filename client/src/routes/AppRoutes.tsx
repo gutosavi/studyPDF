bdc35eaaa78970dfcd4f2c7260479f0b24832daf
Gustavo Savi <gutosavi@hotmail.com>
2026-07-13 12:58:39 -0300

    feat: implementa lazy e Suspense nas rotas

--- client/src/routes/AppRoutes.tsx
+++ client/src/routes/AppRoutes.tsx
@@ -1,15 +1,18 @@
+import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import HomePage from '../pages/HomePage';
-import ChatPage from '../pages/ChatPage';
+const HomePage = React.lazy(() => import('../pages/HomePage'));
+const ChatPage = React.lazy(() => import('../pages/ChatPage'));
 
 export function AppRoutes() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/chat" element={<ChatPage />} />
-      </Routes>
+      <React.Suspense>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/chat" element={<ChatPage />} />
+        </Routes>
+      </React.Suspense>
     </BrowserRouter>
   );
 }
